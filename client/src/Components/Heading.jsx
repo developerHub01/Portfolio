0a3618ff6710485b2b635f5c08368a662e50d6e0
shaki <developerhubytchannel@gmail.com>
2023-12-12 00:38:53 +0600

    skilled section

--- client/src/Components/Heading.jsx
+++ client/src/Components/Heading.jsx
@@ -6,7 +6,9 @@ const Heading = ({ text, align = "left" }) => {
       <h2
         className={`w-fit mx-auto mb-4 text-secondaryColor text-2xl md:text-3xl font-black font-headingFont py-2 border-b-4 border-secondaryColor shadow-xl shadow-secondaryColor capitalize text-${align}`}
       >
-        {`<h2>${text}</h2>`}
+        <span className="lowercase">{`<h2>`}</span>
+        {text}
+        <span className="lowercase">{`</h2>`}</span>
       </h2>
     </div>
   );
--- client/src/Components/Skills.jsx
+++ client/src/Components/Skills.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Container from "./Common/Container";
 import {
   htmlIcon,
   cssIcon,
