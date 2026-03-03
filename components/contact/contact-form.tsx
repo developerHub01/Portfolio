5c178fdee11bcd6ef25bbf7bef202965075a0bc8
developerHub01 <shakil102043@gmail.com>
2026-03-03 19:54:50 +0600

    react-hook-form with zod validation added

--- components/contact/contact-form.tsx
+++ components/contact/contact-form.tsx
@@ -2,74 +2,133 @@
 
 import { PaperAirplaneIcon } from "@/components/icons";
 import FrameV1 from "@/components/ui/frame-v1";
+import { contactSchema } from "@/schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+import { Field, FieldError } from "@/components/ui/field";
+
+interface FormInterface {
+  name: string;
+  email: string;
+  message: string;
+}
 
 const ContactForm = () => {
+  const form = useForm<FormInterface>({
+    resolver: zodResolver(contactSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  const handleSubmit = (data: FormInterface) => {
+    console.log("submitted...");
+    console.log(data);
+  };
+
   return (
     <FrameV1 className="opacity-0 group-hover:opacity-100">
       <form
         className="bg-secondary/30 p-8 sm:p-10 flex flex-col gap-6 shadow-awesome1 border border-border/30"
-        onSubmit={e => {
-          e.preventDefault();
-          const form = e.currentTarget;
-          const name = (form.elements.namedItem("name") as HTMLInputElement)
-            ?.value;
-          const email = (form.elements.namedItem("email") as HTMLInputElement)
-            ?.value;
-          const message = (
-            form.elements.namedItem("message") as HTMLTextAreaElement
-          )?.value;
-          const mailtoLink = `mailto:shakil102043@gmail.com?subject=Portfolio Contact from ${encodeURIComponent(name)}&body=${encodeURIComponent(`From: ${name} (${email})\n\n${message}`)}`;
-          window.open(mailtoLink, "_self");
-        }}
+        // onSubmit={e => {
+        //   e.preventDefault();
+        //   const form = e.currentTarget;
+        //   const name = (form.elements.namedItem("name") as HTMLInputElement)
+        //     ?.value;
+        //   const email = (form.elements.namedItem("email") as HTMLInputElement)
+        //     ?.value;
+        //   const message = (
+        //     form.elements.namedItem("message") as HTMLTextAreaElement
+        //   )?.value;
+        //   const mailtoLink = `mailto:shakil102043@gmail.com?subject=Portfolio Contact from ${encodeURIComponent(name)}&body=${encodeURIComponent(`From: ${name} (${email})\n\n${message}`)}`;
+        //   window.open(mailtoLink, "_self");
+        // }}
+
+        onSubmit={form.handleSubmit(handleSubmit)}
       >
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           <div className="flex flex-col gap-3">
-            <label
-              htmlFor="contact-name"
-              className="text-[10px] font-mono text-muted-foreground uppercase tracking-widest font-bold"
-            >
-              Name
-            </label>
-            <input
-              id="contact-name"
+            <Controller
               name="name"
-              type="text"
-              required
-              placeholder="Your name"
-              className="bg-accent/40 border border-border/40 px-5 py-4 text-base text-foreground placeholder:text-muted-foreground/30 focus:outline-none focus:border-primary transition-all duration-300 shadow-awesome1"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <label
+                    htmlFor="contact-name"
+                    className="text-[10px] font-mono text-muted-foreground uppercase tracking-widest font-bold"
+                  >
+                    Full Name
+                  </label>
+                  <input
+                    id="contact-name"
+                    type="text"
+                    required
+                    placeholder="Your name"
+                    className="bg-accent/40 border border-border/40 px-5 py-4 text-base text-foreground placeholder:text-muted-foreground/30 focus:outline-none focus:border-primary transition-all duration-300 shadow-awesome1 h-auto"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
             />
           </div>
           <div className="flex flex-col gap-3">
-            <label
-              htmlFor="contact-email"
-              className="text-[10px] font-mono text-muted-foreground uppercase tracking-widest font-bold"
-            >
-              Email
-            </label>
-            <input
-              id="contact-email"
+            <Controller
               name="email"
-              type="email"
-              required
-              placeholder="your@email.com"
-              className="bg-accent/40 border border-border/40 px-5 py-4 text-base text-foreground placeholder:text-muted-foreground/30 focus:outline-none focus:border-primary transition-all duration-300 shadow-awesome1"
+              control={form.control}
+              render={({ field, fieldState }) => (
+                <Field>
+                  <label
+                    htmlFor="contact-email"
+                    className="text-[10px] font-mono text-muted-foreground uppercase tracking-widest font-bold"
+                  >
+                    Email
+                  </label>
+                  <input
+                    id="contact-email"
+                    type="email"
+                    required
+                    placeholder="your@email.com"
+                    className="bg-accent/40 border border-border/40 px-5 py-4 text-base text-foreground placeholder:text-muted-foreground/30 focus:outline-none focus:border-primary transition-all duration-300 shadow-awesome1 h-auto"
+                    {...field}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              )}
             />
           </div>
         </div>
         <div className="flex flex-col gap-3">
-          <label
-            htmlFor="contact-message"
-            className="text-[10px] font-mono text-muted-foreground uppercase tracking-widest font-bold"
-          >
-            Message
-          </label>
-          <textarea
-            id="contact-message"
+          <Controller
             name="message"
-            required
-            rows={6}
-            placeholder="Tell me about your project or say hello..."
-            className="bg-accent/40 border border-border/40 px-5 py-4 text-base text-foreground placeholder:text-muted-foreground/30 focus:outline-none focus:border-primary transition-all duration-300 shadow-awesome1 resize-none"
+            control={form.control}
+            render={({ field, fieldState }) => (
+              <Field>
+                <label
+                  htmlFor="contact-message"
+                  className="text-[10px] font-mono text-muted-foreground uppercase tracking-widest font-bold"
+                >
+                  Message
+                </label>
+                <textarea
+                  id="contact-message"
+                  required
+                  rows={6}
+                  placeholder="Tell me about your project or say hello..."
+                  className="bg-accent/40 border border-border/40 px-5 py-4 text-base text-foreground placeholder:text-muted-foreground/30 focus:outline-none focus:border-primary transition-all duration-300 shadow-awesome1 resize-none"
+                  {...field}
+                />
+                {fieldState.invalid && (
+                  <FieldError errors={[fieldState.error]} />
+                )}
+              </Field>
+            )}
           />
         </div>
         <button
---
+++ schema/index.ts
@@ -0,0 +1,21 @@
+import z from "zod";
+
+export const contactSchema = z.object({
+  name: z.string({
+    error: "name is required"
+  }).min(2, {
+    error: "name length must be at least 2"
+  }).max(15, {
+      error: "name length must be lower then 15"
+  }),
+  email: z.email({
+    error: "email is required"
+  }),
+  message: z.string({
+    error: "message is required"
+  }).min(2, {
+    error: "message length must be at least 2"
+  }).max(15, {
+      error: "message length must be lower then 1000"
+  }),
+})
