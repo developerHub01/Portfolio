96592b5b92a1aca32cdffc41f86c489742bba0dc
developerHub01 <shakil102043@gmail.com>
2026-03-04 01:19:42 +0600

    fix: update contactSchema to improve validation for email and trim fields

--- schema/index.ts
+++ schema/index.ts
@@ -5,19 +5,25 @@ export const contactSchema = z.object({
     .string({
       error: "name is required",
     })
+    .trim()
     .min(2, {
       error: "name length must be at least 2",
     })
     .max(15, {
       error: "name length must be lower then 15",
     }),
-  email: z.email({
-    error: "email is required",
-  }),
+  email: z
+    .string({
+      error: "email is required",
+    })
+    .trim()
+    .toLowerCase()
+    .pipe(z.email("Invalid email address")),
   message: z
     .string({
       error: "message is required",
     })
+    .trim()
     .min(2, {
       error: "message length must be at least 2",
     })
