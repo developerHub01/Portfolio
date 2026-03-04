7d9becf8717a73d9a7ce6cbfcb877e3b6f6cb180
developerHub01 <shakil102043@gmail.com>
2026-03-04 16:47:00 +0600

    home social style updated

--- app/(web)/page.tsx
+++ app/(web)/page.tsx
@@ -77,7 +77,7 @@ const HomePage = () => {
                     >
                       <AspectRatio
                         ratio={1}
-                        className="p-0 md:p-3 w-full text-muted-foreground hover:text-primary bg-accent/10 hover:bg-accent transition-all duration-300 shadow-awesome-light border border-transparent hover:border-border/40 grid place-items-center"
+                        className="p-0 w-full text-muted-foreground bg-accent hover:bg-primary hover:text-primary-foreground transition-all duration-300 shadow-awesome-light border border-transparent hover:border-border/40 flex justify-center items-center"
                       >
                         <social.Icon className="size-4 sm:size-5" />
                       </AspectRatio>
